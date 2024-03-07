7519e5b3c2fc65225e7296ba16967562240ccfae
FidelSamuel <samuelrufino020@gmail.com>
2024-03-07 10:58:13 -0300

    teste completo

--- tests/tests.js
+++
@@ -1,9 +0,0 @@
-const { soma, subtracao } = require('../src/calculadora');
-
-test('soma 1 + 2 para ser igual a 3', () => {
-    expect(soma(1, 2)).toBe(3);
-});
-
-test('subtrai 4 - 2 para ser igual a 2', () => {
-    expect(subtracao(4, 2)).toBe(2);
-});
